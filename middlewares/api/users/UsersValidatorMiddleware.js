e5a9bf029df151fb7fdd2d062c4c3573d8c75d5c
usamamashkoor <usamamashkoor@gmail.com>
2021-08-10 04:13:58 +0500

    first commit

--- middlewares/api/users/UsersValidatorMiddleware.js
+++ middlewares/api/users/UsersValidatorMiddleware.js
@@ -1,19 +1,6 @@
 import { check } from 'express-validator';
 import mongoSanitize  from '../../../helpers/sanitization/mongoSanitize.js';
 const UsersValidatorMiddleware = {};
-// class UsersValidatorMiddleware{
-//     static async validate(method){
-//         switch (method) {
-//             case 'registerUser': {
-//                 return [
-//                     check('name', 'Name is required').exists().escape().trim().notEmpty().customSanitizer(mongoSanitize),
-//                     check('email', 'Invalid email').exists().isEmail().escape().trim().notEmpty().customSanitizer(mongoSanitize),
-//                     check('password').isLength({ min: 8 }).exists().escape().trim().notEmpty().customSanitizer(mongoSanitize),
-//                 ]
-//             }
-//         }
-//     }
-// }
 UsersValidatorMiddleware.validate = (method) => {
     switch (method) {
         case 'loginUser': {
@@ -28,17 +15,6 @@ UsersValidatorMiddleware.validate = (method) => {
                 check('last_name', 'last name is required').exists().escape().trim().notEmpty().customSanitizer(mongoSanitize),
                 check('email', 'Invalid email').exists().isEmail().escape().trim().notEmpty().customSanitizer(mongoSanitize),
                 check('password').isLength({ min: 8 }).exists().escape().trim().notEmpty().customSanitizer(mongoSanitize),
-            ]
-        }
-        case 'accountVerifyEmailAddress': {
-            return [
-                check('email_verification_code', 'Email verification code is required').exists().escape().trim().notEmpty().customSanitizer(mongoSanitize),
-                check('email', 'Invalid email').exists().isEmail().escape().trim().notEmpty().customSanitizer(mongoSanitize),
-            ]
-        }
-        case 'accountSendForgotPasswordEmail': {
-            return [
-                check('email', 'Invalid email').exists().isEmail().escape().trim().notEmpty().customSanitizer(mongoSanitize),
             ]
         }
     }
--- routes/users/userApiRoutes.js
+++ routes/users/userApiRoutes.js
@@ -1,9 +1,6 @@
 import express from "express";
-// import {usersValidatorMiddleware} from '../../middlewares/api/users/usersValidatorMiddleware';
-import UsersValidatorMiddleware from '../../middlewares/api/users/UsersValidatorMiddleware.js';
 import UsersApiController from '../../controllers/Users/UsersApiController.js';
 
-import {authMiddleware} from '../../config/passport.js';
 express.application.prefix = express.Router.prefix = function (path, configure) {
     let router = express.Router();
     this.use(path, router);
